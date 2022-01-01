cf9fcf03e7d33a681b0e0fe5fbba2a50dac38d92
Arya Naufal <arya.naufal.andt@gmail.com>
2022-01-01 23:34:08 +0700

    TUGAS 7 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+var tinggi = [140, 145, 150, 155, 160, 165, 170, 175, 180, 185];
+
+for(var x = 0; x < tinggi.length; x++){
+    console.log(tinggi[x])
+}
+
+console.log("")
+
+for(let y of tinggi){
+    console.log(y)
+}
